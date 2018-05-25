60e3abd59c478df387baa024bfa975d4bf36037a
deepak.jaggi@vipps.no <deepak.jaggi@gmail.com>
2018-05-25 09:39:22 +0200

    OK

--- src/main/java/no/vipps/demo/impl/MakePaymentImpl.java
+++ src/main/java/no/vipps/demo/impl/MakePaymentImpl.java
@@ -33,6 +33,30 @@ public class MakePaymentImpl
 		}
 		return makePayment;	
 	}
+	public MakePayment doPaymentMock(MakePayment makePayment)
+	{
+		if (makePayment.getSenderMobileNumber().toString()=="")
+		{
+			makePayment.errorCode="9001";
+			makePayment.errorMessage="sender Number can not be null";			
+		}else if (makePayment.getRxerMobileNumber().toString()=="")
+		{
+			makePayment.errorCode="9002";
+			makePayment.errorMessage="rxer Number can not be null";			
+		}else if (makePayment.getAmount()=="")
+		{
+			makePayment.errorCode="9003";
+			makePayment.errorMessage="amount can not be null";			
+		}
+		else
+		{
+			Random rand=new Random();			
+			makePayment.setErrorCode("9000");
+			makePayment.setErrorMessage("Success");
+			makePayment.setTxnID(Integer.toString(rand.nextInt(100000000)));			
+		}
+		return makePayment;	
+	}
 }
 
 
