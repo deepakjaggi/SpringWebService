1d3901a4350a4e1bbc3f2c1e46fcf9c675f8afab
Deepak Jaggi <deepak.jaggi@banqsoft.no>
2019-01-13 10:42:52 +0100

    OK

--- src/test/java/com/example/demo/RunTests.java
+++ src/test/java/com/example/demo/RunTests.java
@@ -1,16 +1,16 @@
-package com.example.demo;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-//Sample
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class RunTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
+//package com.example.demo;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+////Sample
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class RunTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
